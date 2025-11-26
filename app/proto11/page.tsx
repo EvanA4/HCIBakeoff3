256927da6f588224af6d65ac211ddbe34e2e855c
amy-huang16 <ahuang16@vols.utk.edu>
2025-11-25 20:13:05 -0500

    add round 2 proto scaffolds

---
+++ app/proto11/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { Suspense, useMemo } from "react";
+
+function Proto11Page() {
+  const Proto11 = useMemo(
+    () =>
+      dynamic(() => import("@/components/prototypes/proto11"), {
+        loading: () => (
+          <></>
+        ),
+        ssr: false,
+      }),
+    [],
+  );
+
+  const searchParams = useSearchParams()
+  let dpi = 0;
+  try {
+    const dpiStr = searchParams.get('dpi');
+    if (dpiStr === null) {
+      throw new Error("Invalid DPI in search parameters!");
+    }
+    dpi = parseInt(dpiStr);
+  } catch {
+    return <Link href="/" className="p-3 bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-300 rounded-2xl">Invalid DPI. Click here to return to home.</Link>;
+  }
+
+  return (<Proto11 dpi={dpi}/>);
+}
+
+function Proto11Sus() {
+  return <Suspense>
+    <Proto11Page />
+  </Suspense>
+}
+
+export default Proto11Sus;
---
+++ app/proto8/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { Suspense, useMemo } from "react";
+
+function Proto8Page() {
+  const Proto8 = useMemo(
+    () =>
+      dynamic(() => import("@/components/prototypes/proto8"), {
+        loading: () => (
+          <></>
+        ),
+        ssr: false,
+      }),
+    [],
+  );
+
+  const searchParams = useSearchParams()
+  let dpi = 0;
+  try {
+    const dpiStr = searchParams.get('dpi');
+    if (dpiStr === null) {
+      throw new Error("Invalid DPI in search parameters!");
+    }
+    dpi = parseInt(dpiStr);
+  } catch {
+    return <Link href="/" className="p-3 bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-300 rounded-2xl">Invalid DPI. Click here to return to home.</Link>;
+  }
+
+  return (<Proto8 dpi={dpi}/>);
+}
+
+function Proto8Sus() {
+  return <Suspense>
+    <Proto8Page />
+  </Suspense>
+}
+
+export default Proto8Sus;
