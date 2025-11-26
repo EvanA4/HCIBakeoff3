f18c4d25df67297acf9bedb4d34093e0ae3c1a07
EvanA4 <evanabbott04@gmail.com>
2025-11-26 14:08:02 -0500

    added sorting to prototypes in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,15 @@ function Home() {
     'layout.tsx',
     'page.tsx'
   ];
-  const protoRoutes = readdirSync("./app").filter(name => !ignored.includes(name));
+  const protoRoutes = readdirSync("./app").filter(name => !ignored.includes(name))
+    .sort((a, b) => {
+      if (a === "original")
+        return 1;
+      else if (b === "original")
+        return -1;
+      // console.log(a, b, parseInt(a.substring(5)), parseInt(b.substring(5)))
+      return parseInt(a.substring(5)) > parseInt(b.substring(5)) ? -1 : 1;
+    });
 
   return <ProtoList routes={protoRoutes} />;
 }
